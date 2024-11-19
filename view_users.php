5c37d906ca79ae29a69aa1368f9f1530bebc0a8c
Gabriel Ganderup <gabrielsogaard@gmail.com>
2024-11-19 14:52:01 +0100

    Create view_users.php

---
+++ view_users.php
@@ -0,0 +1,18 @@
+<?php
+include 'dbcon.php';
+
+$sql = "SELECT * FROM user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table border='1'>";
+    echo "<tr><th>Navn</th><th>E-mail</th><th>Alder</th><th>Skostørrelse</th></tr>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr><td>{$row['name']}</td><td>{$row['mail']}</td><td>{$row['age']}</td><td>{$row['shoe_size']}</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "Ingen data fundet.";
+}
+
+$conn->close();
