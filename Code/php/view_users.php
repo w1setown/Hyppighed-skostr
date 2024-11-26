270029603c084b44b6eb9d4c72fe7e73113b6307
Gabriel Ganderup <146817067+w1setown@users.noreply.github.com>
2024-11-26 07:39:45 +0100

    Update ERROR view_users.php

--- Code/php/view_users.php
+++ Code/php/view_users.php
@@ -1,5 +1,11 @@
 <?php
 
+// CHECK ERRORS 
+
+//ini_set('display_errors', '1');
+//ini_set('display_startup_errors', '1');
+//error_reporting(E_ALL);
+
 //include 'dbcon.php';
 require_once 'dbcon.php';
 
