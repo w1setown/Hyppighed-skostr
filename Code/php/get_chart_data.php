a4ad3c193db1f15cf37a4c0645741fae5dfe20c5
Gabriel Ganderup <146817067+w1setown@users.noreply.github.com>
2024-11-26 07:39:26 +0100

    Update get_chart_data.php

--- Code/php/get_chart_data.php
+++ Code/php/get_chart_data.php
@@ -1,10 +1,5 @@
 <?php
 
-// CHECK ERRORS 
-
-//ini_set('display_errors', '1');
-//ini_set('display_startup_errors', '1');
-//error_reporting(E_ALL);
 
 // include "dbcon.php"; | Har skiftet dette da require_once er en mere pålidelig metode
 
