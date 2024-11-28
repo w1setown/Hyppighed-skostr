9a5461df41ecfb16b0f17346bcb00f527484492b
Gabriel Ganderup <gabrielsogaard@gmail.com>
2024-11-28 10:33:44 +0100

    fixes

--- Code/php/get_chart_data.php
+++ Code/php/get_chart_data.php
@@ -1,16 +1,20 @@
-<?php
 
+<?php
 
 // include "dbcon.php"; | Har skiftet dette da require_once er en mere pålidelig metode
 
 require_once 'dbcon.php';
 
-$query = "SELECT shoe_size AS skostorrelse, COUNT(*) AS antal FROM user GROUP BY shoe_size";
+$query = "SELECT shoe_size AS skostorrelse, COUNT(*) AS antal 
+          FROM user 
+          GROUP BY shoe_size 
+          ORDER BY shoe_size ASC"; // ORDER BY = sorteret efter sko størrelser
+
 $result = $conn->query($query);
 
 // Forbered data til grafen
 $data = [];
-$data[] = ['Skostørrelse', 'Antal personer']; // Kolonneoverskrifter
+$data[] = ['Skostørrelse', 'Antal personer']; 
 while ($row = $result->fetch_assoc()) {
     $data[] = [(string)$row['skostorrelse'], (int)$row['antal']];
 }
